8133f381c6cf2a94d9bca8ef513f3d4a5ac004bf
nehalshetty <nehalrshetty@gmail.com>
2020-09-21 11:26:19 +0530

    Css design completed

--- fe/src/components/Login.tsx
+++ fe/src/components/Login.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import axios from "axios";
+import React, { FormEventHandler, useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [user, setUser]: any = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (ev: any) => {
+    ev.preventDefault();
+    let { email, password } = user;
+    try {
+      let userResult = await axios.post("http://localhost:4000/api/login", {
+        email,
+        password,
+      });
+      console.log(userResult);
+    } catch (er) {
+      console.log(er);
+    }
+  };
+
+  const handleInputChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    let newUser: { [key: string]: string } = { ...user };
+
+    newUser[ev.target.name] = ev.target.value;
+
+    setUser(newUser);
+  };
+
   return (
     <div>
-      <div className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="field">
           <input
             type="email"
             name="email"
+            onChange={handleInputChange}
             className="field__input"
-            placeholder="Email"
+            placeholder=" "
           />
           <label htmlFor="email" className="field__label">
             Email
@@ -21,14 +49,20 @@ const Login = () => {
           <input
             type="password"
             name="password"
+            value={user.password}
+            onChange={handleInputChange}
             className="field__input"
-            placeholder="Password"
+            placeholder=" "
           />
           <label htmlFor="password" className="field__label">
             Password
           </label>
         </div>
-      </div>
+
+        <button className="form__button" type="submit">
+          Login
+        </button>
+      </form>
     </div>
   );
 };
